b052db564cde96bcd36579e3251ec5b3885c5cbc
shirenye <305499014@qq.com>
2021-03-31 11:11:56 +0800

    mq更新ssss

--- webgame-mq/src/main/java/com/webgame/mq/Test.java
+++ webgame-mq/src/main/java/com/webgame/mq/Test.java
@@ -1,4 +1,18 @@
 package com.webgame.mq;
 
 public class Test {
+
+    public static void main(String[] args) {
+        int i = 10;
+        if(i == 10) {
+            System.out.println("ssssssss");
+        } else {
+            throw new NullPointerException("qqqqqqqqqq");
+        }
+
+        if(i == 10) {
+            System.out.println("ssssssss");
+        } else {
+            throw new NullPointerException("qqqqqqqqqq");
+        }    }
 }
